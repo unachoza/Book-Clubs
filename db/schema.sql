a1f29f25454c4b73ba537f8a9b0f26e1a5dce690
Arianna Choza <unachoza@gmail.com>
2018-11-20 18:11:50 -0500

    database complete, all tables have entries

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,45 @@
-CREATE TABLE user_profile (
-id BIGSERIAL PRIMARY KEY NOT NULL,
+DROP DATABASE books_dev;
+CREATE DATABASE books_dev;
+\c books_dev;
+
+CREATE TABLE IF NOT EXISTS books (
+id SERIAL PRIMARY KEY NOT NULL,
+title VARCHAR(1000) NOT NULL,
+author VARCHAR(1000) NOT NULL,
+img VARCHAR(1000),
+genre VARCHAR(1000) NOT NULL,
+page_num INT NOT NULL,
+summary VARCHAR(1000) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS book_club (
+id SERIAL PRIMARY KEY, 
+bc_name VARCHAR(1000) NOT NULL,
+bc_description VARCHAR(1000) NOT NULL, 
+bc_location VARCHAR(1000) NOT NULL, 
+bc_book_id INT REFERENCES books(id) NOT NULL,
+readers VARCHAR(1000) NOT NULL,
+readers_emails VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_profile (
+id SERIAL PRIMARY KEY NOT NULL,
 user_name VARCHAR(15) NOT NULL,
 user_password VARCHAR(8) NOT NULL, 
 email VARCHAR(30) NOT NULL, 
-user_location VARCHAR(255) NOT NULL, 
-books VARCHAR(255)  REFERENCES books(id) NOT NULL,
-book_club VARCHAR(255) REFERENCES ref(book_id) NOT NULL
-)
-CREATE TABLE book_club (
-id BIGSERIAL PRIMARY KEY REFERENCES ref(club_id) NOT NULL,
-bc_name VARCHAR(255) NOT NULL,
-bc_description VARCHAR(255) NOT NULL, 
-bc_location VARCHAR(255) NOT NULL, 
-bc_book VARCHAR(255) REFERENCES books(id) NOT NULL,
-details VARCHAR(255) NOT NULL,
-readers VARCHAR(255) NOT NULL,
-readers_emails VARCHAR(255) NOT NULL,
-)
+user_location VARCHAR(1000) NOT NULL, 
+books INT REFERENCES books(id) NOT NULL,
+book_club INT REFERENCES book_club(id) NOT NULL
+);
 
-CREATE TABLE books (
-id BIGSERIAL PRIMARY KEY NOT NULL,
-title VARCHAR(255) NOT NULL,
-author VARCHAR(255) NOT NULL,
-genre VARCHAR(255) NOT NULL,
-page_num INT NOT NULL,
-summary VARCHAR(255) NOT NULL
-)
 
-CREATE TABLE ref (
-book_club_id INT NOT NULL,
-book_id INT NOT NULL,
-)
+
+
+
+-- CREATE TABLE IF NOT EXISTS ref (
+-- ref_id SERIAL PRIMARY KEY,
+-- book_club_id INT REFERENCES book_club(id) NOT NULL,
+-- book_id INT REFERENCES book(id) NOT NULL
+-- );
+
--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,65 @@
+\c books_dev
+
+-- INSERT INTO books ( title, author, img, genre, page_num, summary) VALUES 
+--     (
+--     'The Psychology of Optimal Experience',
+--     'Mihaly Csikszentmihalyi',
+--     '',
+--     'Psychology', 
+--     322, 
+--     'Flow," an experience is a singularly productive and desirable state. But the implications for its application to society are what make the book revolutionary.' 
+--     ),
+--     ( 
+--     'Monster: The Autobiography Of An L.A. Gang Member',
+--     'Sanyika Shakur', 
+--     '',
+--     'Memior', 
+--     400, 
+--     'After pumping eight blasts from a sawed-off shotgun at a group of rival gang members, twelve-year-old Kody Scott was initiated into the L.A. gang the Crips. '
+--     ),
+--     ( 
+--     'Conflict Is Not Abuse: Overstating Harm, Community Responsibility', 'Sarah Schulman', 
+--     '',
+--     'Social Psychology', 
+--     301,
+--     'Difference between Conflict and Abuse. Reveals how punishment replaces personal and collective self-criticism, and shows why difference is so often used to justify cruelty and shunning.'
+--     );
 
 
-INSERT INTO user_profile VALUES (DEFAULT, 'Arianna', 'qqqqqq', 'unachoza@gmail.com', 'Brooklyn', 'Flow: The Psychology of Optimal Experience', 'Social Science Book Club')
-INSERT INTO user_profile VALUES (DEFAULT, 'Paul', 'pppppp', 'pauulchoza@gmail.com', 'Santa Monica', 'Monster: The Autobiography Of An L.A. Gang Member', 'Memior Book Club')
-INSERT INTO user_profile VALUES (DEFAULT, 'Andra', 'aaaaaa', 'andra.lally@gmail.com', 'New York', 'Conflict Is Not Abuse: Overstating Harm, Community Responsibility, and the Duty of Repair ', 'Socialist Feminist Political Education ')
+INSERT INTO user_profile ( user_name, user_password, email, user_location, books, book_club) VALUES 
+    (
+    'Arianna',
+    'qqqqqq', 
+    'unachoza@gmail.com',
+    'Brooklyn',
+    1, 
+    1
+    ),
+    (
+    'Paul', 
+    'pppppp', 
+    'pauulchoza@gmail.com', 
+    'Santa Monica', 
+    2, 
+    1
+    ),
+    ( 
+    'Andra', 
+    'aaaaaa', 
+    'andra.lally@gmail.com', 
+    'New York', 
+    3, 
+    1
+    );
 
-INSERT INTO books VALUES (DEFAULT, 'The Psychology of Optimal Experience', )
-INSERT INTO books VALUES (DEFAULT, 'Monster: The Autobiography Of An L.A. Gang Member','Sanyika Shakur', 'Memior', 'Shakur Sanyika’s Monster was the first one to provide an account of this lifestyle')
-INSERT INTO books VALUES (DEFAULT, 'Conflict Is Not Abuse: Overstating Harm, Community Responsibility')
+
+
+-- INSERT INTO book_club ( bc_name, bc_description, bc_location, bc_book_id, readers, readers_emails) VALUES 
+-- (
+--     'Memior Book Club', 
+--     'We meet once a week at talk about the lives of remarkable memiorist', 
+--     'Starbucks', 
+--     2,
+--     'paul',
+--     'pauulchoza@gmail.com'
+--     );
