442b300a25fcf84bbbdfb71d1f53eaa2b22d10f8
Arianna Choza <unachoza@gmail.com>
2018-11-20 15:36:38 -0500

    adding seeds

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO user_profile VALUES (DEFAULT, 'Arianna', 'qqqqqq', 'unachoza@gmail.com', 'Brooklyn', 'Flow: The Psychology of Optimal Experience', 'Social Science Book Club')
+INSERT INTO user_profile VALUES (DEFAULT, 'Paul', 'pppppp', 'pauulchoza@gmail.com', 'Santa Monica', 'Monster: The Autobiography Of An L.A. Gang Member', 'Memior Book Club')
+INSERT INTO user_profile VALUES (DEFAULT, 'Andra', 'aaaaaa', 'andra.lally@gmail.com', 'New York', 'Conflict Is Not Abuse: Overstating Harm, Community Responsibility, and the Duty of Repair ', 'Socialist Feminist Political Education ')
+
+INSERT INTO books VALUES (DEFAULT, 'The Psychology of Optimal Experience', )
+INSERT INTO books VALUES (DEFAULT, 'Monster: The Autobiography Of An L.A. Gang Member','Sanyika Shakur', 'Memior', 'Shakur Sanyika’s Monster was the first one to provide an account of this lifestyle')
+INSERT INTO books VALUES (DEFAULT, 'Conflict Is Not Abuse: Overstating Harm, Community Responsibility')
