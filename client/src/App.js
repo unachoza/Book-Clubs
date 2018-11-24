4942fba366418e71490f518aa5c91e992b5952dc
Arianna Choza <unachoza@gmail.com>
2018-11-24 10:27:23 -0500

    switch and links working

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,23 @@ import './App.css';
 import Search from './components/Search'
 import CreateAccount from './components/CreateAccount';
 import CreateBookClub from './components/CreateBookClub'
-import{BrowserRouter, Link, Route } from 'react-router-dom'
+import {Switch, Route, Link } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
       <div>
-      <h1>Hello there</h1>
-      
-        <Link to='/Search'>Search</Link>
-        <Route path='/' exact strict render={ () => {
-                    return(<h1>new page</h1>)
-                    }
-                }/>
-      
-      <Search/>
-      <CreateAccount/>
-      <CreateBookClub/>
+        <h1>header</h1>
+        <Link to="/createBookClub"><button>Create Book Club</button></Link>
+        <Switch>
+          <Route path="/createBookClub" component={CreateBookClub}/>
+          <Route path="/" component={Search} />
+        </Switch>
+
+        <h1>footer</h1>
       </div>
-      </BrowserRouter>
+      
     );
   }
 }
